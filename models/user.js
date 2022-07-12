39f7f7972ece8c1073dbb3a1b5cf8be26385a7fd
amandacorral07 <amandalewis0707@gmail.com>
2022-07-12 15:37:23 -0700

    Added Sign In and Login Route and Page

---
+++ models/user.js
@@ -0,0 +1,31 @@
+/////////////////////////////////
+// First, import dependencies
+/////////////////////////////////
+const mongoose= require('./connection')
+
+/////////////////////////////////
+// define our user model
+/////////////////////////////////
+// pull the schema and model constructors from mongoose
+// here, we'll use destructuring syntax to accomplish this
+const { Schema, model } = mongoose
+
+const userSchema = new Schema ({
+    username:{
+        type: String, 
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String, 
+        required: true
+    }
+})
+
+// make a user model with the userSchema
+const User = model('User', userSchema)
+
+/////////////////////////////////
+// exports our user model
+/////////////////////////////////
+module.exports= User
