7afa7cfb1d11dc17cd0752313bd41894bb710609
amandacorral07 <amandalewis0707@gmail.com>
2022-07-13 07:57:42 -0700

    Added succesful working signup and login page

--- server.js
+++ server.js
@@ -5,9 +5,10 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-// const methodOverride = require('method-override')
-const userRoutes = require('./controller/user_routes.js')
-// const userRoutes = require('./controller/user_routes')
+const methodOverride = require('method-override')
+// const fruitRoutes = require('./controller/fruit_routes')
+const userRoutes = require('./controller/user_routes')
+// const commentRoutes = require('./controller/comment_routes')
 
 ////////////////////////////////////////////
 // Create our express application object
@@ -19,34 +20,34 @@ const app = require('liquid-express-views')(express())
 ////////////////////////////////////////////
 // this is for request logging
 app.use(morgan('tiny'))
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 // parses urlencoded request bodies
 app.use(express.urlencoded({ extended: false }))
 // to serve files from public statically
 app.use(express.static('public'))
 // bring in our session middleware 
-// const session = require('express-session')
-// const MongoStore= require('connect-mongo')
+const session = require('express-session')
+const MongoStore= require('connect-mongo')
 
 // here's the middleware that sets up our sessions
-// app.use(
-// 	session({
-// 		secret: process.env.SECRET,
-// 		store: MongoStore.create({
-// 			mongoUrl: process.env.DATABASE_URI
-// 		}),
-// 		saveUninitialized: true,
-// 		resave: false
-// 	})
-// )
+app.use(
+	session({
+		secret: process.env.SECRET,
+		store: MongoStore.create({
+			mongoUrl: process.env.DATABASE_URI
+		}),
+		saveUninitialized: true,
+		resave: false
+	})
+)
 
 ////////////////////////////////////////////
 // Routes
 ////////////////////////////////////////////
 // set up middleware to use route controllers
-app.use('/',userRoutes)
-// app.use('/users', userRoutes)
-
+// app.use('/fruits', fruitRoutes)
+app.use('/users', userRoutes)
+// app.use('/comments', commentRoutes)
 //localhost:3000
 app.get('/', (req, res) => {
 	res.send('your server is running, better go catch it')
